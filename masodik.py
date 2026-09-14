0a47eb442268337783f6a895ee127f4b75d15985
SzLaci22 <gprolaszlo@gmail.com>
2026-09-14 10:31:31 +0200

    labor vége

--- masodik.py
+++ masodik.py
@@ -1,2 +1,15 @@
 #Ez a második labor feladatait tartalmazza
-print("Szia!")
+elhasznalo_kora = int(15.65)
+elhasznalo_kora = int(input("Hány éves vagy: "))
+elhasznalo_kora += 1
+felhasznalo_neve = input('Kérem a nevet: ')
+elhasznalo_kora *= 2
+metszet = felhasznalo_neve[:-5]
+jegyek = [2, 5, 4, 3]
+jegyek += [5]
+del jegyek[0]
+halmaz={'magyar', 'angol', 'orosz', 3}
+hallgato = {"nev": 'Jolán', "kor": 19}
+print(hallgato["nev"])
+print(halmaz)
+print('Szia!', felhasznalo_neve, "!", jegyek)
